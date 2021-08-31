a10bf1586e05e9a269580bacf135d01b021bf6b1
Nizamuddin Mondal <89808462+Nizam420@users.noreply.github.com>
2021-08-31 14:10:34 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about componenet
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Dolor, sequi accusamus illum quas delectus, 
+                ea maiores sed non fugiat enim laboriosam iste amet, 
+                officia odit magnam atque? Culpa veniam ullam eum placeat.</p>
+        </div>
+    )
+}
